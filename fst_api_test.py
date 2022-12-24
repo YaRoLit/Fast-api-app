0dcbdfaf6087590c8e24e834b4d7cda82a7ef810
t-linguist <92468855+t-linguist@users.noreply.github.com>
2022-12-24 15:05:14 +0300

    Add files via upload

--- fst_api_test.py
+++ fst_api_test.py
@@ -4,14 +4,40 @@ from fastapi.testclient import TestClient
 client = TestClient(f.app)
 
 def test_root():
-    info = '- /chats/ \
-        - /reload/\
-        - /upload/\
-        - /addlink/\
-        - body form == {"link": "**filename or chat_link**"}'
-    assert f.root() == {"message": info}
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.json() == {"message": f.info}
+
+def test_find():
+    response = client.get('/chats/')
+    assert response.status_code == 200
+#    assert response.
 
 def test_reload():
-    assert f.reload() == {"message": "chats uploading now. You have to wait some time for search uploading files."}
+    response = client.get('/reload/')
+    assert response.status_code == 200
+    assert response.json() == {"message": "chats uploading now. You have to wait some time for search uploading files."}
 
-    
+def test_upload():
+    response = client.post(
+        '/upload/',
+        json={'item': 'https://t.me/skazochnyy_les'}
+        )
+    assert response.status_code == 200
+#    assert response.
+
+def test_addlink_new():
+    response = client.post(
+        '/addlink/',
+        json={'item': 'https://t.me/hp_main'}
+        )
+    assert response.status_code == 200
+    assert response.json() == {"message": "OK"}
+
+def test_addlink_old():
+    response = client.post(
+        '/addlink/',
+        json={'item': 'https://t.me/skazochnyy_les'}
+        )
+    assert response.status_code == 200
+    assert response.json() == {"message": "This link already exist"}
